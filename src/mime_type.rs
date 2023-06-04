2f91229e27d4b666ef75ca37b508abcb8fd2686c
Shamyl Zakariya <shamyl@gmail.com>
2023-06-04 14:30:07 -0700

    Add mime type wildcards

--- src/mime_type.rs
+++ src/mime_type.rs
@@ -31,9 +31,23 @@ impl MimeType {
         &self.id[0..slash_pos]
     }
 
-    pub fn sub_type(&self) -> &str {
+    pub fn minor_type(&self) -> &str {
         let slash_pos = self.id.find('/').expect("Mimetype should contain a '/'");
         &self.id[slash_pos + 1..self.id.len()]
+    }
+
+    /// True if the minor type is `*`, e.g., `image/*`
+    pub fn is_minor_type_wildcard(&self) -> bool {
+        self.minor_type() == "*"
+    }
+
+    /// True if this MimeType has a wildcard minor type, and the passed-in MimeType matches
+    pub fn wildcard_match(&self, other: &MimeType) -> bool {
+        if self.is_minor_type_wildcard() {
+            self.major_type() == other.major_type()
+        } else {
+            false
+        }
     }
 }
 
@@ -481,7 +495,25 @@ mod tests {
 
         let mime = MimeType::parse("foo/bar")?;
         assert_eq!(mime.major_type(), "foo");
-        assert_eq!(mime.sub_type(), "bar");
+        assert_eq!(mime.minor_type(), "bar");
+
+        Ok(())
+    }
+
+    #[test]
+    fn mime_type_components_work() -> anyhow::Result<()> {
+        let image_png = MimeType::parse("image/png")?;
+        assert_eq!(image_png.major_type(), "image");
+        assert_eq!(image_png.minor_type(), "png");
+        assert!(!image_png.is_minor_type_wildcard());
+
+        let image_star = MimeType::parse("image/*")?;
+        assert_eq!(image_star.major_type(), "image");
+        assert_eq!(image_star.minor_type(), "*");
+        assert!(image_star.is_minor_type_wildcard());
+
+        assert!(image_star.wildcard_match(&image_png));
+        assert!(!image_png.wildcard_match(&image_star));
 
         Ok(())
     }
