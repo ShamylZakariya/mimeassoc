d9be5f93bc9e106fafc3c2ef11224e277a2f5fc5
Shamyl Zakariya <shamyl@gmail.com>
2023-06-20 15:16:57 -0700

    MimeAssocStores::new() was swallowing errors and panicking instead of trickling up

--- src/bin/mimeassoc_gui/stores.rs
+++ src/bin/mimeassoc_gui/stores.rs
@@ -18,35 +18,11 @@ impl std::fmt::Debug for MimeAssocStores {
 
 impl MimeAssocStores {
     pub fn new() -> anyhow::Result<Self> {
-        let desktop_entry_dirs = match desktop_entry_dirs() {
-            Ok(desktop_entry_dirs) => desktop_entry_dirs,
-            Err(e) => panic!("Unable to load desktop_entry_dirs: {:?}", e),
-        };
+        let mime_associations_store = MimeAssociationStore::load(&mimeapps_lists_paths()?)?;
 
-        let mimeapps_lists = match mimeapps_lists_paths() {
-            Ok(mimeapps_lists) => mimeapps_lists,
-            Err(e) => panic!("Unable to load mimeapps_lists_paths: {:?}", e),
-        };
+        let desktop_entry_store = DesktopEntryStore::load(&desktop_entry_dirs()?)?;
 
-        let mime_info_paths = match mimeinfo_paths() {
-            Ok(paths) => paths,
-            Err(e) => panic!("Unable to load mimeinfo_paths: {:?}", e),
-        };
-
-        let mime_associations_store = match MimeAssociationStore::load(&mimeapps_lists) {
-            Ok(mimeassoc) => mimeassoc,
-            Err(e) => panic!("Unable to load Store: {:?}", e),
-        };
-
-        let desktop_entry_store = match DesktopEntryStore::load(&desktop_entry_dirs) {
-            Ok(desktop_entries) => desktop_entries,
-            Err(e) => panic!("Unable to load DesktopEntryStore: {:?}", e),
-        };
-
-        let mime_info_store = match MimeTypeInfoStore::load(&mime_info_paths) {
-            Ok(mime_info_db) => mime_info_db,
-            Err(e) => panic!("Unable to load MimeTypeInfoStore: {:?}", e),
-        };
+        let mime_info_store = MimeTypeInfoStore::load(&mimeinfo_paths()?)?;
 
         Ok(Self {
             mime_associations_store,
